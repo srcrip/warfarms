cb557248bfa79fd896992d4da4b4a56d41fc4502
sevensidedmarble <andrewianstewart@gmail.com>
2019-04-15 12:14:18 -0400

    Add seasonal descriptions

--- src/views/Forest.vue
+++ src/views/Forest.vue
@@ -3,8 +3,7 @@
 div
   h1.title The Red Forest
   h2.subtitle {{ mainDescription }}
-  h4 {{ season }}
-  <!-- h3.subtitle {{ seasonalDescription($store.state.count) }} -->
+  h2.subtitle {{ seasonalDescription }}
 </template>
 
 <script>
@@ -14,12 +13,32 @@ export default {
   name: 'forest',
   computed: {
     mainDescription: () => "Fir trees expand away from you in every direction. The forest floor is a soft bedding of pine needles and peat moss.",
-    seasonalDescription: (season) => ({
-      "Spring": "The spring air is fresh and lively.",
-      "Summer": "The summer air is hot and oppresive.",
-      "Fall": "The fall air is crisp and blowing.",
-      "Winter": "The winter air is bitter and biting."
-    })[season],
+    seasonalDescription: function() {
+      let desc;
+      switch (this.season) {
+        case "Spring":
+          desc = "The spring air is fresh and lively.";
+          break;
+        case "Summer":
+          desc = "The summer air is hot and oppresive.";
+          break;
+        case "Fall":
+          desc = "The fall air is crisp and blowing.";
+          break;
+        case "Winter":
+          desc = "The winter air is bitter and biting.";
+      }
+      return desc;
+      },
+
+    //// Couldn't make this work but might be worth a shot
+    // (season) => ({
+    //   "Spring": "The spring air is fresh and lively.",
+    //   "Summer": "The summer air is hot and oppresive.",
+    //   "Fall": "The fall air is crisp and blowing.",
+    //   "Winter": "The winter air is bitter and biting."
+    // })[season],
+
     ...mapState(['season'])
   }
 }
